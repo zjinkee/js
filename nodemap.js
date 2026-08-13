4eec382a67f737e29a4a659c5a04818cb023b339
zjinkee <z-jinke@qq.com>
2026-08-13 20:23:44 +0800

    Refactor variable names and improve readability

--- nodemap.js
+++ nodemap.js
@@ -14,11 +14,11 @@ $httpClient.get({
         return;
     }
 
-    var json=JSON.parse(body);
+    var inip = JSON.parse(body);
 
-    var host=$environment.params.nodeInfo.address;
-    host=host.split(":")[0];
 
+    
+    var host = $environment.params.nodeInfo.address.split(":")[0];
 
     $httpClient.get({
         url:"https://223.5.5.5/resolve?name="+host+"&type=A",
@@ -28,36 +28,30 @@ $httpClient.get({
         }
     },function(error,response,body){
 
-        var dns=JSON.parse(body);
-        var ip = dns.Answer && dns.Answer.length > 0 
-            ? dns.Answer[0].data 
-            : null;
-
+        var dns = JSON.parse(body);
+        var ip = dns.Answer && dns.Answer.length > 0 ? dns.Answer[0].data : null;
 
         $httpClient.get({
             url:"http://ip-api.com/json/"+ip+"?lang=zh-CN",
             timeout:5000
         },function(error,response,body){
 
-            var ip=JSON.parse(body);
-            var region=new Intl.DisplayNames(['zh-CN'],{
-                type:'region'
-            });
-
+            var outp = JSON.parse(body);
+            var region = new Intl.DisplayNames(['zh-CN'],{ type:'region' });
 
             var html=`
 <p style="text-align:center;font-family:-apple-system;line-height:1.5;">
 <br>
 
 <span style="font-size:14px;font-weight:500;color:#FF9500;">入口位置</span><br>
-<span style="font-size:15px;">IP：${ip.query}</span><br>
-<span style="font-size:14px;">位置：${region.of(ip.countryCode)} ${ip.city}</span><br>
-<span style="font-size:14px;">服务：${ip.isp}</span><br><br>
+<span style="font-size:15px;">IP：${inip.query}</span><br>
+<span style="font-size:14px;">位置：${region.of(inip.countryCode)} ${inip.city}</span><br>
+<span style="font-size:14px;">服务：${inip.isp}</span><br><br>
 
 <span style="font-size:14px;font-weight:500;color:#007AFF;">落地位置</span><br>
-<span style="font-size:15px;">IP：${json.query}</span><br>
-<span style="font-size:14px;">位置：${region.of(json.countryCode)} ${json.city}</span><br>
-<span style="font-size:14px;">服务：${json.isp}</span><br><br>
+<span style="font-size:15px;">IP：${outp.query}</span><br>
+<span style="font-size:14px;">位置：${region.of(outp.countryCode)} ${outp.city}</span><br>
+<span style="font-size:14px;">服务：${outp.isp}</span><br><br>
 
 <span style="font-size:14px;font-weight:500;color:#8659E8;">节点：${node}</span>
 
