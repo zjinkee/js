e4588e18acbf39641c4aea4e3c6772eac2cc72b7
zjinkee <z-jinke@qq.com>
2026-08-13 20:28:32 +0800

    Fix URL format and improve output display logic

--- nodemap.js
+++ nodemap.js
@@ -32,21 +32,24 @@ $httpClient.get({
         var ip = dns.Answer && dns.Answer.length > 0 ? dns.Answer[0].data : null;
 
         $httpClient.get({
-            url:"http://ip-api.com/json/"+ip+"?lang=zh-CN",
+            url:"http://ip-api.com/json/"+ip+"?lang":"zh-CN",
             timeout:5000
         },function(error,response,body){
 
             var outp = JSON.parse(body);
             var region = new Intl.DisplayNames(['zh-CN'],{ type:'region' });
-
+            
+            var showIn = inip.query != outp.query;
             var html=`
 <p style="text-align:center;font-family:-apple-system;line-height:1.5;">
 <br>
 
+${showIn ? `
 <span style="font-size:14px;font-weight:500;color:#FF9500;">入口位置</span><br>
 <span style="font-size:15px;">IP：${inip.query}</span><br>
 <span style="font-size:14px;">位置：${region.of(inip.countryCode)} ${inip.city}</span><br>
 <span style="font-size:14px;">服务：${inip.isp}</span><br><br>
+` : ''}
 
 <span style="font-size:14px;font-weight:500;color:#007AFF;">落地位置</span><br>
 <span style="font-size:15px;">IP：${outp.query}</span><br>
